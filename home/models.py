2fd28fbc3567bb27c7f0cd05e45084aa732bebd8
AdamChrimes <iChrimes@live.co.uk>
2020-04-11 12:23:58 +0100

    :sparkles: add all NHS.UK frontend components to the Homepage

--- home/models.py
+++ home/models.py
@@ -2,15 +2,50 @@ from wagtail.admin.edit_handlers import FieldPanel, StreamFieldPanel
 from wagtail.core.models import Page
 from wagtail.core.fields import StreamField
 
-from wagtailnhsukfrontend.blocks import ActionLinkBlock, WarningCalloutBlock
+from wagtailnhsukfrontend.mixins import (
+    HeroMixin,
+    ReviewDateMixin,
+)
 
-class HomePage(Page):
+from wagtailnhsukfrontend.blocks import (
+    ActionLinkBlock,
+    CareCardBlock,
+    DetailsBlock,
+    DoBlock,
+    DontBlock,
+    ExpanderBlock,
+    ExpanderGroupBlock,
+    GreyPanelBlock,
+    InsetTextBlock,
+    ImageBlock,
+    PanelBlock,
+    PanelListBlock,
+    WarningCalloutBlock,
+    PromoBlock,
+    PromoGroupBlock,
+    SummaryListBlock,
+)
+
+class HomePage(HeroMixin, ReviewDateMixin, Page):
   body = StreamField([
-      # Include any of the blocks you want to use.
       ('action_link', ActionLinkBlock()),
-      ('callout', WarningCalloutBlock()),
+      ('care_card', CareCardBlock()),
+      ('details', DetailsBlock()),
+      ('do_list', DoBlock()),
+      ('dont_list', DontBlock()),
+      ('expander', ExpanderBlock()),
+      ('expander_group', ExpanderGroupBlock()),
+      ('inset_text', InsetTextBlock()),
+      ('image', ImageBlock()),
+      ('panel', PanelBlock()),
+      ('panel_list', PanelListBlock()),
+      ('grey_panel', GreyPanelBlock()),
+      ('warning_callout', WarningCalloutBlock()),
+      ('summary_list', SummaryListBlock()),
   ])
 
-  content_panels = Page.content_panels + [
+  content_panels = Page.content_panels + HeroMixin.content_panels + [
       StreamFieldPanel('body'),
   ]
+
+  settings_panels = Page.settings_panels + ReviewDateMixin.settings_panels
